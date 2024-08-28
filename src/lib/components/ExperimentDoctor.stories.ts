e740e715aaa321e09bb3fca6faf890e91eb361b6
misavojte <mich.vojtechovska@gmail.com>
2024-08-28 15:28:27 +0100

    :refactor Path change

--- src/lib/components/ExperimentDoctor.stories.ts
+++ src/lib/components/ExperimentDoctor.stories.ts
@@ -1,22 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
-import ExperimentDoctorSingleImage from './ExperimentDoctorSingleImage.svelte';
+import ExperimentDoctor from './ExperimentDoctor.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-	title: 'Experiment/ExperimentDoctorSingleImage',
-	component: ExperimentDoctorSingleImage,
+	title: 'Experiment/ExperimentDoctor',
+	component: ExperimentDoctor,
 	tags: ['autodocs']
-} satisfies Meta<ExperimentDoctorSingleImage>;
+} satisfies Meta<ExperimentDoctor>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
-	args: {
-		pictureBase: 'stimuli',
-		pictureId: 'p_00a_f',
-		picureExtension: 'png',
-		pictureCorrection: 0.15
-	}
+	args: {}
 };
--- src/lib/components/ExperimentDoctorImage.stories.ts
+++ src/lib/components/ExperimentDoctorImage.stories.ts
@@ -1,17 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
-import ExperimentDoctorSingle from './ExperimentDoctorSingle.svelte';
+import ExperimentDoctorImage from './ExperimentDoctorImage.svelte';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-	title: 'Experiment/ExperimentDoctorSingle',
-	component: ExperimentDoctorSingle,
+	title: 'Experiment/ExperimentDoctorImage',
+	component: ExperimentDoctorImage,
 	tags: ['autodocs']
-} satisfies Meta<ExperimentDoctorSingle>;
+} satisfies Meta<ExperimentDoctorImage>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
-	args: {}
+	args: {
+		pictureBase: 'stimuli',
+		pictureId: 'p_00a_f',
+		picureExtension: 'png',
+		pictureCorrection: 0.15
+	}
 };
